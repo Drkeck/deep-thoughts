1952486df9c69a8a152030a1c28b701174e3aff5
Drkeck <k3ckl3y.a@gmail.com>
2020-08-01 14:44:14 -0700

    added the post build call for heroku

--- client/src/components/ReactionForm/index.js
+++ client/src/components/ReactionForm/index.js
@@ -36,7 +36,7 @@ const ReactionForm = ({ thoughtId }) => {
     <div>
       <p className={`m-0 ${characterCount === 280 || error ? 'text-error': ''}`}>
         Character Count: {characterCount}/280
-        {/* {error && <span className="ml-2">Something went wrong...</span>} */}
+        {error && <span className="ml-2">Something went wrong...</span>}
 
       </p>
       <form className="flex-row justify-center justify-space-between-md align-stretch"
